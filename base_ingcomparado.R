e2d70448d323557f77bb9f231cfe16320b183aa1
sebastian <sebas262729@gmail.com>
2024-01-28 13:41:55 -0500

    Cambios

--- base_ingcomparado.R
+++ base_ingcomparado.R
@@ -36,7 +36,7 @@ bases <- list(ENEMDU_2015, ENEMDU_2016)
 
 vars <- function(data) {
   return(data[, c("p02", "p01", "p03", "p06", "p24", "p45", "ingrl", "nnivins", 
-                  "periodo", "rama1", "p60a", "p60b",
+                  "periodo", "rama1", "p60a", "p60b", "fexp",
                   "p60c","p60d","p60e","p60f","p60g","p60h","p60i","p60j","p60k")])
 }
 
@@ -64,14 +64,15 @@ df_enemdu <- ENEMDU_FIN %>%
             "Descontento_ingresos_bajos"= 'p60a',
             'Descontento_muchash_trabajo' = 'p60b',
             'Descontento_horarios' = 'p60c',
-            'descontento_sobrecarga' = 'p60d',
+            'Descontento_sobrecarga' = 'p60d',
             'Descontento_inestabilidad' = 'p60e',
             'Descontento_ambiente' = 'p60f',
             'Descontento_trabajo_calle' = 'p60g',
             'Descontento_accidente' = 'p60h',
             'Descontento_actividades' = 'p60i',
             'Descontento_progreso' = 'p60j',
-            'Descontento_malas_rel_lab' = 'p60k') %>%
+            'Descontento_malas_rel_lab' = 'p60k',
+            "fexp") %>%
   filter(edad >= 18, edad <= 65, 
          horas_trabajadas > 0, horas_trabajadas < 999,
          experiencia_laboral > 0, experiencia_laboral < 99,
@@ -84,8 +85,41 @@ df_enemdu <- ENEMDU_FIN %>%
                                                                                    "Básica ",
                                                                                    "Media",
                                                                                    "Superior")),
+         Descontento_ingresos_bajos = factor(Descontento_ingresos_bajos, levels = c(1:2), labels = c(1,0)),
+         Descontento_muchash_trabajo = factor(Descontento_muchash_trabajo, levels = c(1:2), labels = c(1,0)),
+         Descontento_horarios = factor(Descontento_horarios, levels = c(1:2), labels = c(1,0)),
+         Descontento_sobrecarga = factor(Descontento_sobrecarga, levels = c(1:2), labels = c(1,0)),
+         Descontento_inestabilidad = factor(Descontento_inestabilidad, levels = c(1:2), labels = c(1,0)),
+         Descontento_ambiente = factor(Descontento_ambiente, levels = c(1:2), labels = c(1,0)),
+         Descontento_trabajo_calle = factor(Descontento_trabajo_calle, levels = c(1:2), labels = c(1,0)),
+         Descontento_accidente = factor(Descontento_accidente, levels = c(1:2), labels = c(1,0)),
+         Descontento_actividades = factor(Descontento_actividades, levels = c(1:2), labels = c(1,0)),
+         Descontento_progreso = factor(Descontento_progreso, levels = c(1:2), labels = c(1,0)),
+         Descontento_malas_rel_lab = factor(Descontento_malas_rel_lab, levels = c(1:2), labels = c(1,0)),
          ciiu4 = as.factor(ciiu),
-         fecha_1= as.Date(paste0(ano, "01"), format = "%Y%m%d"))
+         fecha_1= as.Date(paste0(ano, "01"), format = "%Y%m%d"),
+         ciiu4_fct = fct_collapse(ciiu4,
+                                  "Agricultura, ganadería,  silvicultura y pesca" = "1",
+                                  "Explotación de minas y canteras" = "2",
+                                  "Industrias manufactureras" = "3",
+                                  "Suministro de electricidad, gas, vapor y aire acondicionado" = "4",
+                                  "Distribución de agua; alcantarillado, gestión de desechos y actividades de saneamiento." = "5",
+                                  "Construcción" = "6",
+                                  "Comercio al por mayor y al por menor; reparación de vehículos automotores y motocicletas" = "7",
+                                  "Transporte y almacenamiento" = "8",
+                                  "Actividades de alojamiento y de servicio de comidas" = "9",
+                                  "Información y comunicación" = "10",
+                                  "Actividades financieras y de seguros" = "11",
+                                  "Actividades inmobiliarias" = "12",
+                                  "Actividades profesionales, científicas y técnicas" = "13",
+                                  "Actividades de servicios administrativos y de apoyo" = "14",
+                                  "Administración pública y defensa; planes de seguridad social de afiliación obligatoria" = "15",
+                                  "Enseñanza" = "16",
+                                  "Actividades de atención de la salud humana y de asistencia social" = "17",
+                                  "Artes, entretenimiento y recreación" = "18",
+                                  "Otras actividades de servicios" = "19",
+                                  "Actividades de los hogares como empleadores; actividades no diferenciadas de los hogares como productores de bienes y servicios para uso propio" = "20",
+                                  "Organizaciones internacionales" = "21"))
 
 
 
